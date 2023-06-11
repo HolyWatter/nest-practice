3af5751fc6e2336f67bdc5be15d265938393580b
HolyWatter <qktjdtn97@icloud.com>
2023-06-11 22:28:50 +0900

    http exception(에러 예외처리)추가

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { HTTPExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  app.useGlobalFilters(new HTTPExceptionFilter());
   //cors 설정
   app.enableCors({
     origin: true,
